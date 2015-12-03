984ea392c045c244320643665ebdc6699ce91d83
ToniRib <tonimarierib@gmail.com>
2015-12-02 20:11:33 -0700

    docs: Set up test files for features tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,16 @@ ENV['RACK_ENV'] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
 require 'minitest/pride'
+requrie 'capybara'
 
 class Minitest::Test
   def teardown
     RobotManager.delete_all
   end
+end
+
+Capybara.app = RobotManagerApp
+
+class FeatureTest < Minitest::Test
+  include Capybara::DSL
 end
