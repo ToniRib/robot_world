4f1c355134a1d0804032dc6d31d3afa53fb5882c
ToniRib <tonimarierib@gmail.com>
2015-12-03 08:19:59 -0700

    test: adds test for viewing one robot's info

---
+++ test/features/user_can_update_an_existing_robot_test.rb
@@ -0,0 +1,27 @@
+require_relative '../test_helper'
+
+class UserCanRegisterANewRobotTest < FeatureTest
+  def test_user_registers_a_new_robot_with_correct_attributes
+    RobotManager.create({ 'id'         => 1,
+                          :name        => 'Frank',
+                          :city        => 'Denver',
+                          :state       => 'CO',
+                          :avatar      => 'https://robohash.org/bluerobot',
+                          :birthdate   => '2014-09-18',
+                          :hired_on    => '2015-1-10',
+                          :department  => 'service'
+                        })
+
+    visit '/robots/1/edit'
+
+    fill_in 'robot[name]', with: 'Frank_v2'
+
+    click_button 'Submit'
+
+    assert_equal '/robots/1', current_path
+
+    within 'h1' do
+      assert page.has_content?('Frank_v2')
+    end
+  end
+end
---
+++ test/features/user_can_view_an_existing_robots_info_test.rb
@@ -0,0 +1,33 @@
+require_relative '../test_helper'
+
+class UserCanViewAnExistingRobotsInfoTest < FeatureTest
+  def test_user_sees_robots_current_information
+    RobotManager.create({ 'id'         => 1,
+                          :name        => 'Frank',
+                          :city        => 'Denver',
+                          :state       => 'CO',
+                          :avatar      => 'https://robohash.org/bluerobot',
+                          :birthdate   => '2014-09-18',
+                          :hired_on    => '2015-1-10',
+                          :department  => 'service'
+                        })
+
+    visit '/robots/1'
+
+    within 'header' do
+      assert page.has_content?('Frank')
+    end
+
+    within '#robot-info' do
+      assert page.has_content?('Denver')
+      assert page.has_content?('CO')
+      assert page.has_content?('2014-09-18')
+      assert page.has_content?('2015-1-10')
+      assert page.has_content?('service')
+    end
+
+    within '#robot-img' do
+      assert page.has_css?("img[src$='https://robohash.org/bluerobot']")
+    end
+  end
+end
