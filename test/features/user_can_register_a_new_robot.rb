230e52bf2d8a1f660b83480fd4ad838abadc7114
ToniRib <tonimarierib@gmail.com>
2015-12-03 08:05:30 -0700

    test: feature test for registering a robot

---
+++ test/features/user_can_register_a_new_robot.rb
@@ -0,0 +1,23 @@
+require_relative '../test_helper'
+
+class UserCanRegisterANewRobotTest < FeatureTest
+  def test_user_registers_a_new_robot_with_correct_attributes
+    visit '/robots/new'
+
+    fill_in 'robot[name]', with: 'Frank'
+    fill_in 'robot[city]', with: 'Denver'
+    fill_in 'robot[state]', with: 'CO'
+    fill_in 'robot[avatar]', with: 'https://robohash.org/bluerobot'
+    fill_in 'robot[birthdate]', with: '2014-09-18'
+    fill_in 'robot[hired_on]', with: '2015-1-10'
+    choose 'Service'
+
+    click_button 'Submit'
+
+    assert_equal '/robots', current_path
+
+    within '.card' do
+      assert page.has_content?('Frank')
+    end
+  end
+end
