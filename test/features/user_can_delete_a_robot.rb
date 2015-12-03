7ed963f309f02262a507c1304efb44adeb02f767
ToniRib <tonimarierib@gmail.com>
2015-12-03 07:58:30 -0700

    test: Adds tests for deleting a robot

---
+++ test/features/user_can_delete_a_robot.rb
@@ -0,0 +1,23 @@
+require_relative '../test_helper'
+
+class UserCanDeleteARobotTest < FeatureTest
+  def test_user_deletes_an_existing_robot
+    RobotManager.create({ 'id'         => 1,
+                          :name        => 'Frank',
+                          :city        => 'Denver',
+                          :state       => 'CO',
+                          :avatar      => 'https://robohash.org/bluerobot',
+                          :birthdate   => '2014-09-18',
+                          :hired_on    => '2015-1-10',
+                          :department  => 'service'
+                        })
+
+    visit '/robots'
+
+    click_button 'delete'
+
+    within '.container' do
+      refute page.has_content?('Frank')
+    end
+  end
+end
